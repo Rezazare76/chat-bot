86f3280b46aed605c365dfd8a1e3b12dd1a74437
Reza Zare <rezazare2088@gmail.com>
2024-02-21 09:12:48 +0330

    ✨ feat(add chatBot component):

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Chat } from "videorepute-chat-interview";
+interface messageType {
+  text: string;
+  position: "user" | "bot";
+  type: "text" | "image" | "pdf";
+  time: string;
+}
+function App() {
+  const storedData = localStorage.getItem("dataMessage");
+  const saveMessage = (elm: messageType[]) => {
+    localStorage.setItem("dataMessage", JSON.stringify(elm));
+  };
+  const clearMessage = () => {
+    localStorage.removeItem("dataMessage");
+  };
+  return (
+    <Chat
+      messageList={storedData ? JSON.parse(storedData) : undefined}
+      greetingMessage="hello Welcome to chat bot"
+      positions={{ right: "10px", bottom: "10px" }}
+      logo=""
+      clear={clearMessage}
+      onChange={(elm) => saveMessage(elm)}
+      dir="rtl"
+    />
+  );
+}
+
+export default App;
